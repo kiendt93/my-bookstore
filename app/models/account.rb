28923775d1ac2648a97eb7ab82bf946f95308e32
kiendt <kiendt0893@gmail.com>
2015-05-08 20:33:27 +0700

    day 8-5

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,7 @@ class Account < ActiveRecord::Base
 	has_many :orders, dependent: :destroy
 
 	validates :username, :password, presence: true, length: { in: 6..20 }
-	validates :age, numericlity: { only_integer: true }
+	validates :age, numericality: { only_integer: true }
 	validates :email, email: true, allow_blank: true
 	validates :first_name, :last_name, allow_blank: false
 end
